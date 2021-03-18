b32f01adbea0750a7b79920ff711a03e89d03cdc
VizorTank <36300779+VizorTank@users.noreply.github.com>
2021-03-18 16:21:32 +0100

    Dodaj pliki projektów.

---
+++ Zad3/Pages/Index.cshtml.cs
@@ -0,0 +1,52 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel;
+
+namespace Zad3.Pages
+{
+    public class IndexModel : PageModel
+    {
+        private readonly ILogger<IndexModel> _logger;
+
+        [BindProperty, Range(1, 1000, ErrorMessage = "Liczby of 1 do 1000")]
+        public int BuzzFizz { get; set; }
+
+        public IndexModel(ILogger<IndexModel> logger)
+        {
+            _logger = logger;
+        }
+
+        public void OnGet()
+        {
+
+        }
+
+        public IActionResult OnPost()
+        {
+            if (ModelState.IsValid)
+            {
+                String output = "";
+                if (BuzzFizz % 3 == 0)
+                    output = "" + "Fizz";
+                if (BuzzFizz % 5 == 0)
+                    output = output + "Buzz";
+                if (output == "")
+                    output = "Liczba: " + BuzzFizz + " nie spełnia kryteriów Fizz/Buzz";
+                else
+                    output = "Otrzymano: " + output;
+
+                HttpContext.Session.SetString("BuzzFizz", output);
+                return RedirectToPage("./Szukane");
+            }
+            return Page();
+        }
+    }
+}
---
+++ Zad3/Pages/Szukane.cshtml.cs
@@ -0,0 +1,20 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.AspNetCore.Http;
+
+
+namespace Zad3.Pages
+{
+    public class SzukaneModel : PageModel
+    {
+        public String BuzzFizz { get; set; }
+        public void OnGet()
+        {
+            BuzzFizz = HttpContext.Session.GetString("BuzzFizz");
+        }
+    }
+}
